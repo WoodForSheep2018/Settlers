3caff4e30350225cb4f53f5588e4511e58290cd9
ajthomas99 <39067514+ajthomas99@users.noreply.github.com>
2018-05-11 13:31:28 -0700

    Update Hex.java

--- Hex.java
+++ Hex.java
@@ -1,49 +1,58 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.ArrayList;
 
-public class Hex {
-	public enum Resource{Wood, Brick, Sheep, Wheat, Rock}
-	private Resource type;
+public abstract class Hex {
 	private int xLoc;
 	private int yLoc;
 	private int[] xPoints;
 	private int[] yPoints;
+	private ArrayList<Location> vertices = new ArrayList<Location>();
 	private int width;
 	private int height;
 
-	public Hex(Resource r, int x, int y, int w) {
-		type = r;
+	public Hex(int x, int y, int w) {
 		xLoc = x;
 		yLoc = y;
 		width = w;
 		height = (int)(2*(width/Math.sqrt(3)));
 		xPoints = new int[] {xLoc, xLoc + width/2, xLoc + width/2, xLoc, xLoc - width/2, xLoc - width/2};
-		yPoints = new int[] {yLoc + height/2,
-				     yLoc + height/4,
-				     yLoc - height/4,
-				     yLoc - height/2,
-				     yLoc - height/4,
-				     yLoc + height/4};
+		yPoints = new int[] {yLoc + height/2, yLoc + height/4, yLoc - height/4, yLoc - height/2, yLoc - height/4, yLoc + height/4};
+		
+		for(int i = 0; i < xPoints.length; i++) {
+			vertices.add(new Location(xPoints[i], yPoints[i]));
+		}
 	}
 
-	public void draw(Graphics g) {
-		if(type == Resource.Wood) {
-			g.setColor(new Color(0,100,0));
-		}
-		if(type == Resource.Brick) {
-			g.setColor(new Color(204,0,0));
-		}
-		if(type == Resource.Sheep) {
-			g.setColor(new Color(0,255,0));
-		}
-		if(type == Resource.Wheat) {
-			g.setColor(new Color(204,204,0));
-		}
-		if(type == Resource.Rock) {
-			g.setColor(new Color(96,96,96));
-		}
-		g.fillPolygon(xPoints, yPoints, 6);
+	public abstract void draw(Graphics g);
+	
+	public int[] getXPoints() {
+		return xPoints;
 	}
-
+	
+	public int[] getYPoints() {
+		return yPoints;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getXLoc() {
+		return xLoc;
+	}
+	
+	public int getYLoc() {
+		return yLoc;
+	}
+	
+	public ArrayList<Location> getVertices() {
+		return vertices;
+	}
+	
 }
