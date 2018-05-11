521e497c745161839c18d38acc50fb338a582c59
ajthomas99 <39067514+ajthomas99@users.noreply.github.com>
2018-05-11 13:59:25 -0700

    Add files via upload

---
+++ DevelopmentCard.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class DevelopmentCard {
+	
+	public enum DevCardTypes{
+		Knight, VictoryPoint, RoadBuilding, Monopoly, YearOfPlenty
+	}
+	private DevCardTypes type;
+	
+	public DevelopmentCard(DevCardTypes t) {
+		type=t;
+	}
+	
+	public void play() {
+		if(type.equals(DevCardTypes.Knight)) {
+			
+		}
+		else if(type.equals(DevCardTypes.VictoryPoint)) {
+			
+		}
+		else if(type.equals(DevCardTypes.RoadBuilding)) {
+			
+		}
+		else if(type.equals(DevCardTypes.Monopoly)) {
+	
+		}
+		else if(type.equals(DevCardTypes.YearOfPlenty)) {
+			
+		}
+
+	}
+	
+	public DevCardTypes getType() {
+		return type;
+	}
+}
