c1ccd26ee8c24fd800a7ebff2d69d032f8582290
ajthomas99 <39067514+ajthomas99@users.noreply.github.com>
2018-05-12 23:04:18 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Board {
 	private Hex[][] hexArr = new Hex[7][];
@@ -8,7 +9,11 @@ public class Board {
 	private int boardXLoc;
 	private int hexWidth;
 	private int hexHeight;
-	private int gap;
+	private int xGap;
+	private ArrayList<OceanHex.Port> availablePorts = new ArrayList<OceanHex.Port>();
+	private ArrayList<TerrainHex.Resource> availableTiles = new ArrayList<TerrainHex.Resource>();
+	private ArrayList<Integer> availableNumbers = new ArrayList<Integer>();
+	private boolean isDesert = false;
 
 	public Board(int pw, int ph) {
 		hexArr[0] = new Hex[4];
@@ -18,51 +23,165 @@ public class Board {
 		hexArr[4] = new Hex[6];
 		hexArr[5] = new Hex[5];
 		hexArr[6] = new Hex[4];
-		
+
 		boardWidth = pw/2;
 		boardHeight = (3*ph)/4;
 		boardXLoc = pw/4;
 		hexWidth = boardWidth/7;
 		hexHeight = (int)(2*(hexWidth/Math.sqrt(3)));
-		gap = boardWidth - 7*hexWidth;
-		
+		xGap = boardWidth - 7*hexWidth;
+
+		availablePorts.add(OceanHex.Port.Wood);
+		availablePorts.add(OceanHex.Port.Brick);
+		availablePorts.add(OceanHex.Port.Wheat);
+		availablePorts.add(OceanHex.Port.Sheep);
+		availablePorts.add(OceanHex.Port.Rock);
+		availablePorts.add(OceanHex.Port.ThreeOne);
+		availablePorts.add(OceanHex.Port.ThreeOne);
+		availablePorts.add(OceanHex.Port.ThreeOne);
+		availablePorts.add(OceanHex.Port.ThreeOne);
+
+		availableTiles.add(TerrainHex.Resource.Wood);
+		availableTiles.add(TerrainHex.Resource.Wood);
+		availableTiles.add(TerrainHex.Resource.Wood);
+		availableTiles.add(TerrainHex.Resource.Wood);
+		availableTiles.add(TerrainHex.Resource.Brick);
+		availableTiles.add(TerrainHex.Resource.Brick);
+		availableTiles.add(TerrainHex.Resource.Brick);
+		availableTiles.add(TerrainHex.Resource.Sheep);
+		availableTiles.add(TerrainHex.Resource.Sheep);
+		availableTiles.add(TerrainHex.Resource.Sheep);
+		availableTiles.add(TerrainHex.Resource.Sheep);
+		availableTiles.add(TerrainHex.Resource.Wheat);
+		availableTiles.add(TerrainHex.Resource.Wheat);
+		availableTiles.add(TerrainHex.Resource.Wheat);
+		availableTiles.add(TerrainHex.Resource.Wheat);
+		availableTiles.add(TerrainHex.Resource.Rock);
+		availableTiles.add(TerrainHex.Resource.Rock);
+		availableTiles.add(TerrainHex.Resource.Rock);
+		availableTiles.add(TerrainHex.Resource.Desert);
+
+		availableNumbers.add(2);
+		availableNumbers.add(3);
+		availableNumbers.add(3);
+		availableNumbers.add(4);
+		availableNumbers.add(4);
+		availableNumbers.add(5);
+		availableNumbers.add(5);
+		availableNumbers.add(6);
+		availableNumbers.add(6);
+		availableNumbers.add(8);
+		availableNumbers.add(8);
+		availableNumbers.add(9);
+		availableNumbers.add(9);
+		availableNumbers.add(10);
+		availableNumbers.add(10);
+		availableNumbers.add(11);
+		availableNumbers.add(11);
+		availableNumbers.add(12);
+
 		setTileLocs();
+		setUpPorts();
 	}
-		
+
 	public void setTileLocs() {
 		int y = hexHeight/2;
 		int x = boardXLoc;
 		for(int r = 0; r < hexArr.length; r++) {
 			if(hexArr[r].length == 4) {
-				x = boardXLoc + gap/2 + 2*hexWidth;
+				x = boardXLoc + xGap/2 + 2*hexWidth;
 			}
 			else if(hexArr[r].length == 5) {
-				x = boardXLoc + gap/2 + (3*hexWidth)/2;
+				x = boardXLoc + xGap/2 + (3*hexWidth)/2;
 			}
 			else if(hexArr[r].length == 6) {
-				x = boardXLoc + gap/2 + hexWidth;
+				x = boardXLoc + xGap/2 + hexWidth;
 			}
 			else if(hexArr[r].length == 7) {
-				x = boardXLoc + gap/2 + hexWidth/2;
+				x = boardXLoc + xGap/2 + hexWidth/2;
 			}
 			for(int c = 0; c < hexArr[r].length; c++) {
 				if(r == 0 || r == hexArr.length - 1 || c == 0 || c == hexArr[r].length - 1) {
-					hexArr[r][c] = new OceanHex(OceanHex.Port.Sheep, x, y, hexWidth);
+					hexArr[r][c] = new OceanHex(OceanHex.Port.Ocean, x, y, hexWidth);
 				}
 				else {
-					hexArr[r][c] = new TerrainHex(TerrainHex.Resource.Wheat, x, y, hexWidth, 5);
+					hexArr[r][c] = new TerrainHex(randomTile(), x, y, hexWidth, randomNumber());
 				}
 				x += hexWidth;
 			}
 			y += (3*hexHeight)/4;
 		}
 	}
-	
+
+	public void setUpPorts() {
+		OceanHex.Port currentPort;
+		int randIndex;
+
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[0][0] = new OceanHex(currentPort, hexArr[0][0].getXLoc(), hexArr[0][0].getYLoc(), hexWidth);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[0][2] = new OceanHex(currentPort, hexArr[0][2].getXLoc(), hexArr[0][2].getYLoc(), hexWidth);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[1][4] = new OceanHex(currentPort, hexArr[1][4].getXLoc(), hexArr[1][4].getYLoc(), hexWidth);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[2][0] = new OceanHex(currentPort, hexArr[2][0].getXLoc(), hexArr[2][0].getYLoc(), hexWidth);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[3][6] = new OceanHex(currentPort, hexArr[3][6].getXLoc(), hexArr[3][6].getYLoc(), hexWidth);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[4][0] = new OceanHex(currentPort, hexArr[4][0].getXLoc(), hexArr[4][0].getYLoc(), hexWidth);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[5][4] = new OceanHex(currentPort, hexArr[5][4].getXLoc(), hexArr[5][4].getYLoc(), hexWidth);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[6][0] = new OceanHex(currentPort, hexArr[6][0].getXLoc(), hexArr[6][0].getYLoc(), hexWidth);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[6][2] = new OceanHex(currentPort, hexArr[6][2].getXLoc(), hexArr[6][2].getYLoc(), hexWidth);
+	}
+
+	public TerrainHex.Resource randomTile() {
+		int randIndex = (int)(Math.random()*availableTiles.size());
+		TerrainHex.Resource temp = availableTiles.get(randIndex);
+		if(temp == TerrainHex.Resource.Desert) {
+			isDesert = true;
+		}
+		availableTiles.remove(randIndex);
+		return temp;
+	}
+
+	public int randomNumber() {
+		if(isDesert == true) {
+			isDesert = false;
+			return 0;
+		}
+		else {
+			int randIndex = (int)(Math.random()*availableNumbers.size());
+			int temp = availableNumbers.get(randIndex);
+			availableNumbers.remove(randIndex);
+			return temp;
+		}
+	}
+
 	public void draw(Graphics g) {
 		g.setColor(new Color(153,255,255));
 		g.fillRect(boardXLoc, 0, boardWidth, boardHeight);
-		
-		
+
 		for(int r = 0; r < hexArr.length; r++) {
 			for(int c = 0; c < hexArr[r].length; c++) {
 				hexArr[r][c].draw(g);
