8cc21440c5425645d46da9cfe179a1a1a9cef937
ajthomas99 <39067514+ajthomas99@users.noreply.github.com>
2018-05-22 21:38:31 -0700

    Update Building.java

--- Building.java
+++ Building.java
@@ -1,30 +1,57 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Building {
 
-  private int level;
-  private Player owner;
-  
-  public Building(Player p) {
-    level = 0;
-    owner=p;
-  
-  }
-  public void upgrade() {
-    if(level >= 2) {
-      System.out.println("Cannot do");
-      return;
-    }
-    level++;
-  }
-  
-  public boolean isASettlement() { return level==1; }
-  public boolean isACity() { return level==2; }
-  
-  public void setOwner(Player owner) { this.owner = owner; }
-  public Player getOwner() { return owner; }
-  
-  public void give(TerrainHex.Resource resource) {
-    for(int n = 0;n < level;n++) owner.addCard(resource);
-  }
-  
+	private int level;
+	private Player owner;
+	private int xLoc;
+	private int yLoc;
+	private int size;
+
+	public Building(int x, int y, int hexWidth) {
+		level = 0;
+		xLoc = x;
+		yLoc = y;
+		size = hexWidth/6;
+	}
+
+	public void buildSettlement() {
+		level = 1;
+	}
+	
+	public void buildCity() {
+		level = 2;
+	}
+
+	public boolean isASettlement() { 
+		return level == 1; 
+	}
+	
+	public boolean isACity() { 
+		return level == 2; 
+	}
+
+	public void setOwner(Player p) { 
+		owner = p; 
+	}
+
+	public Player getOwner() { 
+		return owner; 
+	}
+
+	public void draw(Graphics g) {
+//		g.setColor(owner.getColor());
+		g.setColor(Color.PINK);
+		if(level == 1) {
+			g.fillRect(xLoc - size/2, yLoc - size/2, size, size);
+			g.fillPolygon(new int[]{xLoc-size/2, xLoc, xLoc+size/2}, new int[]{yLoc-size/2, yLoc-size, yLoc-size/2}, 3);
+		}
+		if(level == 2) {
+			g.fillRect(xLoc, yLoc, size, size);
+			g.fillRect(xLoc - size, yLoc - size/2, size, (3*size)/2);
+			g.fillPolygon(new int[]{xLoc-size, xLoc-size/2, xLoc}, new int[]{yLoc-size/2, yLoc-size, yLoc-size/2}, 3);
+		}
+	}
+
 }
