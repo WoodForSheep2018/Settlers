a623a13619af160ddb16d80a2ac237250a3bfc3b
ajthomas99 <39067514+ajthomas99@users.noreply.github.com>
2018-05-22 19:36:39 -0700

    Update Location.java

--- Location.java
+++ Location.java
@@ -1,50 +1,4 @@
-
-public class Location {
-	private int row;
-	private int col;
-	private Player player;
-	private Building building;
-	
-	public Location(int r, int c) {
-		row = r;
-		col = c;
-	}
-	
-	public int getRow() {
-		return row;
-	}
-	
-	public int getCol() {
-		return col;
-	}
-	
-	public boolean hasSettlement(){
-		return building.isASettlement();		
-	}
-	
-	public boolean hasCity(){
-		return building.isACity();
-	}
-	
-	public void makeCity() {
-		building.upgrade();
-	}
-	
-	public void makeSettlement(Player p) {
-		building = new Building(p);
-		
-	}
-	
-	public Player getPlayer() {
-		return player;
-	}
-	
-	public Building getBuilding() {
-		return building;
-	}
-}
-
-/* 
+import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class Location {
@@ -52,26 +6,47 @@ public class Location {
 	private int col;
 	private ArrayList<TerrainHex.Resource> surroundingResources;
 	private ArrayList<Integer> surroundingNums;
-	
-	public Location(int r, int c) {
+	private Building building;
+
+	public Location(int r, int c, int hexSize) {
 		row = r;
 		col = c;
 		surroundingResources = new ArrayList<TerrainHex.Resource>();
 		surroundingNums = new ArrayList<Integer>();
+		building = new Building(r, c, hexSize);
 	}
-	
+
 	public int getRow() {
 		return row;
 	}
-	
+
 	public int getCol() {
 		return col;
 	}
-	
+
 	public void assign(TerrainHex.Resource r, int i) {
 		surroundingResources.add(r);
 		surroundingNums.add(i);
 	}
-}
 
-*/
+	public boolean hasSettlement(){
+		return building.isASettlement();		
+	}
+
+	public boolean hasCity(){
+		return building.isACity();
+	}
+
+	public void makeCity() {
+		building.upgrade();
+	}
+
+	public void makeSettlement() {
+		building.upgrade();
+	}
+	
+	public void draw(Graphics g) {
+		building.draw(g);
+	}
+	
+}
