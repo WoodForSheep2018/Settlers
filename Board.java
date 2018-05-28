3d38010cdf0f0a8687c15d68644dd3b5cb5bb79f
ajthomas99 <39067514+ajthomas99@users.noreply.github.com>
2018-05-28 12:55:19 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,179 +1,298 @@
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
-import javax.swing.AbstractAction;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.KeyStroke;
-import javax.swing.UIManager;
 
-public class SettlersPanel extends JPanel {
-	private final int PANEL_WIDTH = 1200;
-	private final int PANEL_HEIGHT = 800;
-	private Board b;
-	private Player player1;
-	private Player player2;
-	private Player player3;
-	private Player player4;
-	private ArrayList<Player> playerList = new ArrayList<Player>();
-	private PlayerBox box1;
-	private PlayerBox box2;
-	private PlayerBox box3;
-	private PlayerBox box4;
-	private Player currentPlayer;
-	private int turn;
-	private boolean roadAfterSettlement = false;
-	private int settlementX = 0;
-	private int settlementY = 0;
+public class Board {
+	private Hex[][] hexArr = new Hex[7][];
+	private int boardWidth;
+	private int boardHeight;
+	private int boardXLoc;
+	private int hexWidth;
+	private int hexHeight;
+	private int xGap;
+	private int yGap;
+	private ArrayList<OceanHex.Port> availablePorts = new ArrayList<OceanHex.Port>();
+	private ArrayList<TerrainHex.Resource> availableTiles = new ArrayList<TerrainHex.Resource>();
+	private ArrayList<Integer> availableNumbers = new ArrayList<Integer>();
+	private ArrayList<Location> spaces = new ArrayList<Location>();
+	private ArrayList<Road> roads = new ArrayList<Road>();
+	private boolean isDesert = false;
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Catan");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new SettlersPanel());
-		frame.pack();
-		frame.setVisible(true);
-		frame.setResizable(false);
+	public Board(int pw, int ph) {
+		hexArr[0] = new Hex[4];
+		hexArr[1] = new Hex[5];
+		hexArr[2] = new Hex[6];
+		hexArr[3] = new Hex[7];
+		hexArr[4] = new Hex[6];
+		hexArr[5] = new Hex[5];
+		hexArr[6] = new Hex[4];
+
+		boardWidth = pw/2;
+		boardHeight = (4*ph)/5;
+		boardXLoc = pw/4;
+		hexWidth = boardWidth/7;
+		hexHeight = (int)(2*(hexWidth/Math.sqrt(3)));
+		xGap = boardWidth - 7*hexWidth;
+		yGap = boardHeight - (11*hexHeight)/2;
+
+		availablePorts.add(OceanHex.Port.Wood);
+		availablePorts.add(OceanHex.Port.Brick);
+		availablePorts.add(OceanHex.Port.Wheat);
+		availablePorts.add(OceanHex.Port.Sheep);
+		availablePorts.add(OceanHex.Port.Rock);
+		availablePorts.add(OceanHex.Port.ThreeOne);
+		availablePorts.add(OceanHex.Port.ThreeOne);
+		availablePorts.add(OceanHex.Port.ThreeOne);
+		availablePorts.add(OceanHex.Port.ThreeOne);
+
+		availableTiles.add(TerrainHex.Resource.Wood);
+		availableTiles.add(TerrainHex.Resource.Wood);
+		availableTiles.add(TerrainHex.Resource.Wood);
+		availableTiles.add(TerrainHex.Resource.Wood);
+		availableTiles.add(TerrainHex.Resource.Brick);
+		availableTiles.add(TerrainHex.Resource.Brick);
+		availableTiles.add(TerrainHex.Resource.Brick);
+		availableTiles.add(TerrainHex.Resource.Sheep);
+		availableTiles.add(TerrainHex.Resource.Sheep);
+		availableTiles.add(TerrainHex.Resource.Sheep);
+		availableTiles.add(TerrainHex.Resource.Sheep);
+		availableTiles.add(TerrainHex.Resource.Wheat);
+		availableTiles.add(TerrainHex.Resource.Wheat);
+		availableTiles.add(TerrainHex.Resource.Wheat);
+		availableTiles.add(TerrainHex.Resource.Wheat);
+		availableTiles.add(TerrainHex.Resource.Rock);
+		availableTiles.add(TerrainHex.Resource.Rock);
+		availableTiles.add(TerrainHex.Resource.Rock);
+		availableTiles.add(TerrainHex.Resource.Desert);
+
+		availableNumbers.add(2);
+		availableNumbers.add(3);
+		availableNumbers.add(3);
+		availableNumbers.add(4);
+		availableNumbers.add(4);
+		availableNumbers.add(5);
+		availableNumbers.add(5);
+		availableNumbers.add(6);
+		availableNumbers.add(6);
+		availableNumbers.add(8);
+		availableNumbers.add(8);
+		availableNumbers.add(9);
+		availableNumbers.add(9);
+		availableNumbers.add(10);
+		availableNumbers.add(10);
+		availableNumbers.add(11);
+		availableNumbers.add(11);
+		availableNumbers.add(12);
+
+		setTileLocs();
+		setUpPorts();
 	}
 
-	public SettlersPanel() {
-		this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
-		setUpGameObjects();
-		startGame();
-	}
-
-	public void setUpGameObjects() {
-		b = new Board(PANEL_WIDTH, PANEL_HEIGHT);
-		player1 = new Player(b, "Alan", Color.BLUE);
-		playerList.add(player1);
-		player2 = new Player(b, "Devon", Color.RED);
-		playerList.add(player2);
-		player3 = new Player(b, "Payton", Color.PINK);
-		playerList.add(player3);
-		player4 = new Player(b, "Maz", Color.YELLOW);
-		playerList.add(player4);
-		box1 = new PlayerBox(PANEL_WIDTH, PANEL_HEIGHT, 0, 0, player1);
-		box2 = new PlayerBox(PANEL_WIDTH, PANEL_HEIGHT, 0, PANEL_HEIGHT/2, player2);
-		box3 = new PlayerBox(PANEL_WIDTH, PANEL_HEIGHT, (3*PANEL_WIDTH)/4, 0, player3);
-		box4 = new PlayerBox(PANEL_WIDTH, PANEL_HEIGHT, (3*PANEL_WIDTH)/4, PANEL_HEIGHT/2, player4);
-	}
-
-	public void startGame() {
-		turn = 1;
-		pickStartingPlayer();
-		setupMouseListener();
-	}
-
-	public void pickStartingPlayer() {
-		int start = (int)(Math.random()*4 + 1);
-		for(Player p : playerList) {
-			if(p.getPlayerNumber() == start) {
-				currentPlayer = p;
-				currentPlayer.setTurn(true);
+	public void setTileLocs() {
+		int y = yGap/2 + hexHeight/2;
+		int x = boardXLoc;
+		for(int r = 0; r < hexArr.length; r++) {
+			if(hexArr[r].length == 4) {
+				x = boardXLoc + xGap/2 + 2*hexWidth;
 			}
-		}
-	}
-
-	public void nextTurn() {
-		turn++;
-		currentPlayer.setTurn(false);
-		int current = currentPlayer.getPlayerNumber();
-		if(current < 4) {
-			current++;
-		}
-		else {
-			current = 1;
-		}
-		for(Player p : playerList) {
-			if(p.getPlayerNumber() == current) {
-				currentPlayer = p;
-				currentPlayer.setTurn(true);
+			else if(hexArr[r].length == 5) {
+				x = boardXLoc + xGap/2 + (3*hexWidth)/2;
 			}
-		}
-	}
-
-	public void setupMouseListener() {
-		this.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if(turn < 9) {
-					if(roadAfterSettlement == false) {
-						int x = e.getX();
-						int y = e.getY();
-						if(b.closestLoc(x,y) != null) {
-							settlementX = b.closestLoc(x,y).getXLoc();
-							settlementY = b.closestLoc(x,y).getYLoc();
-							if(b.checkAdjacentLocs(b.closestLoc(settlementX, settlementY))) {
-								b.closestLoc(settlementX, settlementY).makeSettlement(currentPlayer);
-								repaint();
-								roadAfterSettlement = true;
-							}
-						}
+			else if(hexArr[r].length == 6) {
+				x = boardXLoc + xGap/2 + hexWidth;
+			}
+			else if(hexArr[r].length == 7) {
+				x = boardXLoc + xGap/2 + hexWidth/2;
+			}
+			for(int c = 0; c < hexArr[r].length; c++) {
+				if(r == 0 || r == hexArr.length - 1 || c == 0 || c == hexArr[r].length - 1) {
+					hexArr[r][c] = new OceanHex(OceanHex.Port.Ocean, x, y, hexWidth);
+				}
+				else {
+					TerrainHex.Resource randTile = randomTile();
+					int randNum = randomNumber();
+					hexArr[r][c] = new TerrainHex(randTile, x, y, hexWidth, randNum);
+					if(randTile == TerrainHex.Resource.Desert) {
+						hexArr[r][c].makeRobber(true);
 					}
-					else if(roadAfterSettlement == true) {
-						int x = e.getX();
-						int y = e.getY();
-						if(b.closestLoc(x,y) != null) {
-							int roadFinalX = b.closestLoc(x,y).getXLoc();
-							int roadFinalY = b.closestLoc(x,y).getYLoc();
-							if(b.isAdjacent(settlementX, settlementY, roadFinalX, roadFinalY)) {
-								b.addRoad(new Road(settlementX, settlementY, roadFinalX, roadFinalY, currentPlayer));
-								repaint();
-								settlementX = 0;
-								settlementY = 0;
-								roadAfterSettlement = false;
-								nextTurn();
-							}
+					for(int i = 0; i < hexArr[r][c].getVertices().size(); i++) {
+						if(!isSameLoc(hexArr[r][c].getVertices().get(i))) {
+							spaces.add(hexArr[r][c].getVertices().get(i));
+							spaces.get(spaces.size() - 1).assign(randTile, randNum);
+							spaces.get(spaces.size() - 1).assignHex(hexArr[r][c]);
+						}
+						else {
+							int index = findSameLoc(hexArr[r][c].getVertices().get(i));
+							spaces.get(index).assign(randTile, randNum);
+							spaces.get(index).assignHex(hexArr[r][c]);
 						}
 					}
 				}
-
-
-
-
+				x += hexWidth;
 			}
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub	
-			}
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub	
-			}
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub	
-			}
-		});
-	}	
+			y += (3*hexHeight)/4;
+		}
+	}
 
-	public boolean gameOver() {
-		for(Player p : playerList) {
-			if(p.getPoints() >= 10) {
-				return true;
+	public void setUpPorts() {
+		OceanHex.Port currentPort;
+		int randIndex;
+
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[0][0] = new OceanHex(currentPort, hexArr[0][0].getXLoc(), hexArr[0][0].getYLoc(), hexWidth);
+		portLocations(hexArr[0][0], currentPort);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[0][2] = new OceanHex(currentPort, hexArr[0][2].getXLoc(), hexArr[0][2].getYLoc(), hexWidth);
+		portLocations(hexArr[0][2], currentPort);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[1][4] = new OceanHex(currentPort, hexArr[1][4].getXLoc(), hexArr[1][4].getYLoc(), hexWidth);
+		portLocations(hexArr[1][4], currentPort);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[2][0] = new OceanHex(currentPort, hexArr[2][0].getXLoc(), hexArr[2][0].getYLoc(), hexWidth);
+		portLocations(hexArr[2][0], currentPort);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[3][6] = new OceanHex(currentPort, hexArr[3][6].getXLoc(), hexArr[3][6].getYLoc(), hexWidth);
+		portLocations(hexArr[3][6], currentPort);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[4][0] = new OceanHex(currentPort, hexArr[4][0].getXLoc(), hexArr[4][0].getYLoc(), hexWidth);
+		portLocations(hexArr[4][0], currentPort);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[5][4] = new OceanHex(currentPort, hexArr[5][4].getXLoc(), hexArr[5][4].getYLoc(), hexWidth);
+		portLocations(hexArr[5][4], currentPort);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[6][0] = new OceanHex(currentPort, hexArr[6][0].getXLoc(), hexArr[6][0].getYLoc(), hexWidth);
+		portLocations(hexArr[6][0], currentPort);
+		randIndex = (int)(Math.random()*availablePorts.size());
+		currentPort = availablePorts.get(randIndex);
+		availablePorts.remove(randIndex);
+		hexArr[6][2] = new OceanHex(currentPort, hexArr[6][2].getXLoc(), hexArr[6][2].getYLoc(), hexWidth);
+		portLocations(hexArr[6][2], currentPort);
+	}
+	
+	public void portLocations(Hex h, OceanHex.Port p) {
+		int samePorts = 0;
+		for(int i = 0; i < spaces.size(); i++) {
+			for(int j = 0; j < h.getVertices().size(); j++) {
+				if(Math.abs(spaces.get(i).getXLoc() - h.getVertices().get(j).getXLoc()) < 3) {
+					if(Math.abs(spaces.get(i).getYLoc() - h.getVertices().get(j).getYLoc()) < 3) {
+						if(samePorts < 2)
+						spaces.get(i).makePort(p);
+						samePorts++;
+					}
+				}
+			}
+		}
+	}
+
+	public TerrainHex.Resource randomTile() {
+		int randIndex = (int)(Math.random()*availableTiles.size());
+		TerrainHex.Resource temp = availableTiles.get(randIndex);
+		if(temp == TerrainHex.Resource.Desert) {
+			isDesert = true;
+		}
+		availableTiles.remove(randIndex);
+		return temp;
+	}
+
+	public int randomNumber() {
+		if(isDesert == true) {
+			isDesert = false;
+			return 0;
+		}
+		else {
+			int randIndex = (int)(Math.random()*availableNumbers.size());
+			int temp = availableNumbers.get(randIndex);
+			availableNumbers.remove(randIndex);
+			return temp;
+		}
+	}
+	
+	public boolean isSameLoc(Location loc) {
+		for(Location temp : spaces) {
+			if(Math.abs(loc.getXLoc() - temp.getXLoc()) < 3) {
+				if(Math.abs(loc.getYLoc() - temp.getYLoc()) < 3) {
+					return true;
+				}
 			}
 		}
 		return false;
 	}
-
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		b.draw(g);
-		box1.draw(g);
-		box2.draw(g);
-		box3.draw(g);
-		box4.draw(g);
+	
+	public int findSameLoc(Location loc) {
+		for(int i = 0; i < spaces.size(); i++) {
+			if(Math.abs(loc.getXLoc() - spaces.get(i).getXLoc()) < 3) {
+				if(Math.abs(loc.getYLoc() - spaces.get(i).getYLoc()) < 3) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(new Color(153,255,255));
+		g.fillRect(boardXLoc, 0, boardWidth, boardHeight);
+		for(int r = 0; r < hexArr.length; r++) {
+			for(int c = 0; c < hexArr[r].length; c++) {
+				hexArr[r][c].draw(g);
+			}
+		}
+		for(Location loc : spaces) {
+			loc.draw(g);
+		}
+		for(Road r : roads) {
+			r.draw(g);
+		}
+	}
+	
+	public boolean checkAdjacentLocs(Location loc) {
+		for(int i = 0; i < spaces.size(); i++) {
+			if((Math.sqrt(Math.pow((loc.getXLoc() - spaces.get(i).getXLoc()), 2) + Math.pow((loc.getYLoc() - spaces.get(i).getYLoc()), 2))) < hexHeight/2 + 5) {
+				if(spaces.get(i).hasSettlement() || spaces.get(i).hasCity()) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
-}
-
+	public Location closestLoc(int x, int y) {
+		for(int i = 0; i < spaces.size(); i++) {
+			if(Math.abs(spaces.get(i).getXLoc() - x) < 20) {
+				if(Math.abs(spaces.get(i).getYLoc() - y) < 20) {
+					return spaces.get(i);
+				}
+			}
+		}
+		return null;
+	}
+	
+	public boolean isAdjacent(int x1, int y1, int x2, int y2) {
+		if((Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))) < hexHeight/2 + 5) {
+				return true;
+		}
+		return false;
+	}
+	
+	public void addRoad(Road r) {
+		roads.add(r);
+	}
 	
 	public void setUpOrDown() {
 		for(Location loc:spaces) {
