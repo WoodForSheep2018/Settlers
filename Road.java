5f86241316e6e97a3f24f0e69dc6860e24d8f362
ajthomas99 <39067514+ajthomas99@users.noreply.github.com>
2018-05-28 12:31:52 -0700

    Add files via upload

---
+++ Road.java
@@ -0,0 +1,34 @@
+import java.awt.Graphics;
+
+public class Road {
+	private int startX;
+	private int startY;
+	private int finalX;
+	private int finalY;
+	private Player owner;
+	private int[] xPoints;
+	private int[] yPoints;
+
+	public Road(int x1, int y1, int x2, int y2, Player p) {
+		startX = x1;
+		startY = y1;
+		finalX = x2;
+		finalY = y2;
+		owner = p;
+
+		if(Math.abs(startX - finalX) < 2) {
+			xPoints = new int[] {startX + 3, finalX + 3, finalX - 3, startX - 3};
+			yPoints = new int[] {startY, finalY, finalY, startY};
+		}
+		else {
+			xPoints = new int[] {startX, finalX, finalX, startX};
+			yPoints = new int[] {startY + 3, finalY + 3, finalY - 3, startY - 3};
+		}
+		
+	}
+
+	public void draw(Graphics g) {
+		g.setColor(owner.getColor());
+		g.fillPolygon(xPoints, yPoints, 4);
+	}
+}
