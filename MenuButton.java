33f91d72966714177055ea6bce461eb87fdf56ab
ajthomas99 <39067514+ajthomas99@users.noreply.github.com>
2018-05-29 17:31:26 -0700

    Update MenuButton.java

--- MenuButton.java
+++ MenuButton.java
@@ -3,10 +3,41 @@ import java.awt.Font;
 import java.awt.Graphics;
 
 public class MenuButton {
-	private int xMin = 300;
-	private int xMax = 900;
-	private int yMin = 640;
-	private int yMax = 800;
+	private int xMin;
+	private int xMax;
+	private int yMin;
+	private int yMax;
+	private int width;
+	private int height;
+	private int boxWidth;
+	private int boxHeight;
+	private int xSpacing;
+	private int ySpacing;
+	
+	private int devCardX;
+	private int roadX;
+	private int settlementX;
+	private int cityX;
+	private int endTurnX;
+	
+	public MenuButton(Board b, int ph) {
+		xMin = b.getBoardXLoc();
+		xMax = b.getBoardXLoc() + b.getBoardWidth();
+		yMin = b.getBoardHeight();
+		yMax = ph;
+		width = xMax - xMin;
+		height = yMax - yMin;
+		boxWidth = width/6;
+		boxHeight = height/6;
+		xSpacing = (width - 4*boxWidth)/5;
+		ySpacing = (height - 2*boxHeight)/3;
+		
+		devCardX = xMin + xSpacing;
+		roadX = devCardX + boxWidth + xSpacing;
+		settlementX = roadX + boxWidth + xSpacing;
+		cityX = settlementX + boxWidth + xSpacing;
+		endTurnX = xMin + width/2 - boxWidth/2;
+	}
 
 	public int getXMin() {
 		return xMin;
@@ -23,23 +54,72 @@ public class MenuButton {
 	public int getYMax() {
 		return yMax;
 	}
+	
+	public boolean devCardBox(int x, int y) {
+		if(x > devCardX && x < devCardX + boxWidth) {
+			if(y > yMin + ySpacing && y < yMin + ySpacing + boxHeight) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean roadBox(int x, int y) {
+		if(x > roadX && x < roadX + boxWidth) {
+			if(y > yMin + ySpacing && y < yMin + ySpacing + boxHeight) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean settlementBox(int x, int y) {
+		if(x > settlementX && x < settlementX + boxWidth) {
+			if(y > yMin + ySpacing && y < yMin + ySpacing + boxHeight) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean cityBox(int x, int y) {
+		if(x > cityX && x < cityX + boxWidth) {
+			if(y > yMin + ySpacing && y < yMin + ySpacing + boxHeight) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean endTurnBox(int x, int y) {
+		if(x > endTurnX && x < endTurnX + boxWidth) {
+			if(y > yMin + 2*ySpacing + boxHeight && y < yMin + 2*ySpacing + 2*boxHeight) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public void draw(Graphics g) {
 		g.setColor(Color.WHITE);
-		g.fillRect(xMin, 640, 600, 160);
+		g.fillRect(xMin, yMin, width, height);
+		
+		g.setColor(Color.PINK);
+		g.fillRect(devCardX, yMin + ySpacing, boxWidth, boxHeight);
+		g.fillRect(roadX, yMin + ySpacing, boxWidth, boxHeight);
+		g.fillRect(settlementX, yMin + ySpacing, boxWidth, boxHeight);
+		g.fillRect(cityX, yMin + ySpacing, boxWidth, boxHeight);
+		
+		g.setColor(Color.ORANGE);
+		g.fillRect(endTurnX, yMin + 2*ySpacing + boxHeight, boxWidth, boxHeight);
 
 		g.setColor(Color.BLACK);
-		g.drawRect(xMin, 640, 150, 120);
-		g.drawRect(450, 640, 150, 120);
-		g.drawRect(600, 640, 150, 120);
-		g.drawRect(750, 640, 150, 120);
-		g.drawRect(xMin, 760, 600, 39);
-		g.setFont(new Font("TimesRoman", Font.PLAIN, 12));
-		g.drawString("Play development card", xMin, 720);
-		g.drawString("Build road", 500, 720);//first click is startLoc, second is endLoc
-		g.drawString("Build settlement", 630, 720);
-		g.drawString("Build city", 790, 720);
-		g.drawString("End turn", 550, 780);
-
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 14));
+		int textHeight = g.getFontMetrics().getAscent();
+		g.drawString("Play Card", devCardX, yMin + ySpacing + textHeight);
+		g.drawString("Build Road", roadX, yMin + ySpacing + textHeight);
+		g.drawString("Build Settlement", settlementX, yMin + ySpacing + textHeight);
+		g.drawString("Build City", cityX, yMin + ySpacing + textHeight);
+		g.drawString("End Turn", endTurnX, yMin + 2*ySpacing + boxHeight + textHeight);
 	}
 }
