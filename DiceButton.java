b4c11aa69e16109f75150a5bfc8f7fd35af4c69e
ajthomas99 <39067514+ajthomas99@users.noreply.github.com>
2018-05-30 17:50:28 -0700

    Update DiceButton.java

--- DiceButton.java
+++ DiceButton.java
@@ -7,19 +7,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class DiceButton extends JPanel{
-	int xMin=300;
-	int xMax=400;
-	int yMin=0;
-	int yMax=100;
-	int roll = 0;
+	private int xMin;
+	private int xMax;
+	private int yMin;
+	private int yMax;
+	private int roll;
 	
-	public DiceButton() {
+	public DiceButton(Board b) {
+		xMin = b.getBoardXLoc();
+		xMax = b.getBoardXLoc() + b.getBoardWidth()/6;
+		yMin = 0;
+		yMax = b.getBoardHeight()/8;
 	}
 	
 	public int roll() {
-		int one = (int) (Math.random()*6) + 1;
-		int two = (int) (Math.random()*6) + 1;
-		roll = one+two;
+		int one = (int)(Math.random()*6) + 1;
+		int two = (int)(Math.random()*6) + 1;
+		roll = one + two;
 		return roll;
 	}
 	
@@ -36,15 +40,20 @@ public class DiceButton extends JPanel{
 		return yMax;
 	}
 
-	
 	public void draw(Graphics g) {
 		g.setColor(Color.WHITE);
-		g.fillRect(xMin, yMin, 100, 100);
-		g.setFont(new Font("TimesRoman", Font.ITALIC, 19));
+		g.fillRect(xMin, yMin, xMax - xMin, yMax - yMin);
 		g.setColor(Color.BLACK);
-		g.drawString("Click to roll", 300, 20);
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 15));
+		int textWidth = g.getFontMetrics().stringWidth("Click to roll");
+		int textHeight = g.getFontMetrics().getAscent();
+		g.drawString("Click to roll", (xMin + xMax)/2 - textWidth/2, yMin + textHeight);
 		g.setFont(new Font("TimesRoman", Font.BOLD, 30));
-		if(roll>0)
-			g.drawString(""+roll, 340, 60);
+		if(roll > 0) {
+			int numWidth = g.getFontMetrics().stringWidth("" + roll);
+			int numHeight = g.getFontMetrics().getAscent();
+			g.drawString("" + roll, (xMin + xMax)/2 - numWidth/2, yMax - numHeight/2);
+		}
 	}
+	
 }
